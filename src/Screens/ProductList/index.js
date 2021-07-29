8ff12e2de52029fe536bc04967f90f081f441269
FaisalKhawaj <56731703+FaisalKhawaj@users.noreply.github.com>
2021-07-29 15:33:16 +0500

    ios

--- src/Screens/ProductList/index.js
+++ src/Screens/ProductList/index.js
@@ -1,21 +1,21 @@
-import React, { useEffect,useCallback, useState } from 'react'
+import React, { useEffect, useCallback, useState } from 'react'
 import { Container, Content } from 'native-base';
-import { View, Text, TouchableOpacity,FlatList, Dimensions, Image, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, Dimensions, Image, StyleSheet } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import ToggleSwitch from 'toggle-switch-react-native'
 import HeaderWithBelowText from '../../Components/BackBtnTextBelowText';
 import LinearGradient from 'react-native-linear-gradient';
 import Realm from 'realm'
-import {PRODUCTS_SCHEMA,DATABASENAME, ProductsSchema} from '../../Database/schema'
+import { PRODUCTS_SCHEMA, DATABASENAME, ProductsSchema } from '../../Database/schema'
 
 const { width, height } = Dimensions.get('window')
 let realm = null;
-import { useIsFocused ,useFocusEffect } from '@react-navigation/native';
+import { useIsFocused, useFocusEffect } from '@react-navigation/native';
 
 const ProductList = ({ navigation }) => {
     const isFocused = useIsFocused()
     const [Product, setProduct] = useState([])
-    
+
 
     const AddProductHandler = () => {
         navigation.navigate('AddProduct')
@@ -24,45 +24,52 @@ const ProductList = ({ navigation }) => {
         useCallback(() => {
             realm = new Realm({
                 path: DATABASENAME,
-                schema:[ProductsSchema]
+                schema: [ProductsSchema]
             });
-            try{
+            try {
                 var productsdetalils = realm.objects(PRODUCTS_SCHEMA);
                 setProduct(productsdetalils)
-            }catch(err){
+            } catch (err) {
                 console.log(err)
             }
         }, [isFocused])
-      );
+    );
     const [Toggle, setToggle] = useState(false)
-    
-    const renderItem = ({ item }) => {
-      
-        return (
-        
-        <View style={{width:width, alignSelf:"center"}}>
-            <View style={styles.productView}>
-                <Image style={{ width: wp('70%'), height:wp("70%"), overflow:"hidden", borderTopLeftRadius:20, borderTopRightRadius:20 }}
-                    resizeMode="cover" source={{uri:item.ProductImage}} />
-                <TouchableOpacity style={{ position: 'absolute', top: 40, right: 10 }}>
-                    <Image source={require('../../assets/images/circledelete.png')} />
-                </TouchableOpacity>
 
-                <View style={styles.productTextToggleView}>
-                    <Text style={{ color: '#474747', fontSize: 18 }}>{item.ProductStep}</Text>
-                    <ToggleSwitch
-                        isOn={Toggle}
-                        onColor="#2575FC"
-                        offColor="black"
-                        labelStyle={{ color: "black", fontWeight: "900" }}
-                        size="small"
-                        onToggle={isOn => setToggle(isOn)}
-                    />
+    const renderItem = ({ item }) => {
+
+        return (
+
+            <View style={{ width: width - 100, alignSelf: "center" }}>
+                <View style={styles.productView}>
+                    <Image style={{
+                        width: wp('60%'), height: wp("60%"),
+                        overflow: "hidden", borderTopLeftRadius: 20, borderTopRightRadius: 20
+                    }}
+                        resizeMode="cover" source={{ uri: item.ProductImage }} />
+                    <TouchableOpacity style={{
+                        position: 'absolute',
+                        top: 10, right: 10
+                    }}>
+                        <Image source={require('../../assets/images/circledelete.png')} />
+                    </TouchableOpacity>
+
+                    <View style={styles.productTextToggleView}>
+                        <Text style={{ color: '#474747', fontSize: 18 }}>{item.ProductStep}</Text>
+                        <ToggleSwitch
+                            isOn={Toggle}
+                            onColor="#2575FC"
+                            offColor="black"
+                            labelStyle={{ color: "black", fontWeight: "900" }}
+                            size="small"
+                            onToggle={isOn => setToggle(isOn)}
+                        />
+                    </View>
                 </View>
             </View>
-        </View>
-    )}
-    
+        )
+    }
+
     return (
         <Container>
             <Content contentContainerStyle={{ flex: 1 }}>
@@ -78,16 +85,16 @@ const ProductList = ({ navigation }) => {
                     start={{ x: 0, y: 0 }} end={{ x: 0, y: 1 }}
                     colors={['#6A11CB', '#2575FC']} style={styles.ProductLinearGradientView}>
 
-                    {Product.length?
-                    <FlatList
-                        data={Product}
-                        horizontal
-                        renderItem={renderItem}
-                        
-                        keyExtractor={item => item.ProductID.toString()}
-                    />
-                    :
-                    <Text style={{color:"#fff", alignSelf:"center", fontSize:20}}>No Products</Text>
+                    {Product.length ?
+                        <FlatList
+                            data={Product}
+                            horizontal
+                            renderItem={renderItem}
+
+                            keyExtractor={item => item.ProductID.toString()}
+                        />
+                        :
+                        <Text style={{ color: "#fff", alignSelf: "center", fontSize: 20 }}>No Products</Text>
                     }
                 </LinearGradient>
 
@@ -101,7 +108,7 @@ const ProductList = ({ navigation }) => {
 
 
             </Content>
-           
+
         </Container>
     )
 }
@@ -112,7 +119,7 @@ const styles = StyleSheet.create({
         borderRadius: 15,
         borderWidth: 1,
         borderColor: '#FFFF',
-        width: wp('70%'),
+        width: wp('60%'),
         backgroundColor: '#FFFF',
         // height: height / 2.4,
         alignSelf: 'center',
